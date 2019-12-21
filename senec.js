c4b5a292fe4e9c9b7f2f0f5632b13f94c0495b86
J.Koeber <johannes.koeber@ancud.de>
2019-12-21 13:53:28 +0100

    fixing project

--- senec.js
+++ senec.js
@@ -45,10 +45,11 @@ function readSettings() {
         return
     }  
     
-    readFromServer();
+    readFromServer(senecIp);
 }
 
-function readFromServer() {	
+function readFromServer(senecIp) {
+	adapter.log.info('send request: http://'+senecIp+'/lala.cgi');
 	request.post('http://'+senecIp+'/lala.cgi', {
 		json: {"STATISTIC":{"STAT_DAY_E_HOUSE":"","STAT_DAY_E_PV":"","STAT_DAY_BAT_CHARGE":"","STAT_DAY_BAT_DISCHARGE":"","STAT_DAY_E_GRID_IMPORT":"","STAT_DAY_E_GRID_EXPORT":"","STAT_YEAR_E_PU1_ARR":""},"ENERGY":{"STAT_STATE":"","STAT_STATE_DECODE":"","GUI_BAT_DATA_POWER":"","GUI_INVERTER_POWER":"","GUI_HOUSE_POW":"","GUI_GRID_POW":"","STAT_MAINT_REQUIRED":"","GUI_BAT_DATA_FUEL_CHARGE":"","GUI_CHARGING_INFO":"","GUI_BOOSTING_INFO":""},"WIZARD":{"CONFIG_LOADED":"","SETUP_NUMBER_WALLBOXES":"","SETUP_WALLBOX_SERIAL0":"","SETUP_WALLBOX_SERIAL1":"","SETUP_WALLBOX_SERIAL2":"","SETUP_WALLBOX_SERIAL3":""}}
 	}, (error, res, body) => {
@@ -58,7 +59,7 @@ function readFromServer() {
 		}
 		
 		// output response to log
-		adapter.log.info('got feedback from senec sevice: ' + body);
+		adapter.log.info('got feedback from senec service: ' + body);
 		
 		// subscribe on all variables of this adapter instance with pattern "adapterName.X.*"
 		adapter.setState('STAT_DAY_E_HOUSE', senec.getVarValue('fl_4179F679'), true);
