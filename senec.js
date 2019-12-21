76773ad68385ab2c0e9e24085cd1610989f5788e
J.Koeber <johannes.koeber@ancud.de>
2019-12-21 13:31:30 +0100

    fixing project

--- senec.js
+++ senec.js
@@ -1,103 +1,27 @@
+"use strict";
+
 const utils = require('@iobroker/adapter-core'); // Get common adapter utils - mandatory
-const adapterName = require('./package.json').name.split('.').pop();
 const senec   = require('./lib/senec');
-const meta   = require('./lib/meta');
+const request = require('request');
 const adapter = utils.adapter('senec'); // - mandatory
-const request = require('request')
 
-let rpc;
-let rpcClient;
-
-let rpcServer;
-
-const metaValues = {};
-let metaRoles = {};
-const dpTypes = {};
-
-function main() {
-	
-	adapter.config.reconnectInterval = parseInt(adapter.config.reconnectInterval, 10) || 30;
-    if (adapter.config.reconnectInterval < 10) {
-        adapter.log.error('Reconnect interval is less than 10 seconds. Set reconnect interval to 10 seconds.');
-        adapter.config.reconnectInterval = 10;
-    }
-	
-	adapter.config.checkInitInterval = parseInt(adapter.config.checkInitInterval, 10);
-    if (adapter.config.checkInitInterval < 10) {
-        adapter.log.error('Check init interval is less than 10 seconds. Set init interval to 10 seconds.');
-        adapter.config.checkInitInterval = 10;
-    }
-
-    adapter.setState('info.connection', false, true);
-	
-    // list devices
-    adapter.objects.getObjectView('system', 'instance', 
-    		{startkey: 'senec.' + adapter.instance + '.', endkey: 'senec.' + adapter.instance + '.\u9999'},
-    	(err, doc) => {
-	        if (err) {
-	        	adapter.log.error(`getObjectView senec: ${err}`);
-	        }
-	        const res = [];
-	        doc.rows.forEach(row => res.push(row.value));
-	        const instances = {};
-	        res.forEach(instance => {
-	            instances[instance.common.name] = {};
-	            instances[instance.common.name].version = instance.common.installedVersion;
-	        });
-	    });
-    
-	/* Load VALUE paramsetDescriptions (needed to create state objects)
-    adapter.getObjectView('system', 'paramsetDescription', {
-        startkey: 'senec.meta.VALUES',
-        endkey: 'senec.meta.VALUES.\u9999'
-    }, (err, doc) => {
-        if (err) adapter.log.error(`getObjectView senec: ${err}`);
-        if (doc && doc.rows) {
-            for (const row of doc.rows) {
-                const channel = row.id.slice(19);
-                metaValues[channel] = row.value.native;
-            }
-        }
-    });
-	*/
-}
-
-function readFromServer() {	
-	request.post('http://192.168.178.31/lala.cgi', {
-		json: {"STATISTIC":{"STAT_DAY_E_HOUSE":"","STAT_DAY_E_PV":"","STAT_DAY_BAT_CHARGE":"","STAT_DAY_BAT_DISCHARGE":"","STAT_DAY_E_GRID_IMPORT":"","STAT_DAY_E_GRID_EXPORT":"","STAT_YEAR_E_PU1_ARR":""},"ENERGY":{"STAT_STATE":"","STAT_STATE_DECODE":"","GUI_BAT_DATA_POWER":"","GUI_INVERTER_POWER":"","GUI_HOUSE_POW":"","GUI_GRID_POW":"","STAT_MAINT_REQUIRED":"","GUI_BAT_DATA_FUEL_CHARGE":"","GUI_CHARGING_INFO":"","GUI_BOOSTING_INFO":""},"WIZARD":{"CONFIG_LOADED":"","SETUP_NUMBER_WALLBOXES":"","SETUP_WALLBOX_SERIAL0":"","SETUP_WALLBOX_SERIAL1":"","SETUP_WALLBOX_SERIAL2":"","SETUP_WALLBOX_SERIAL3":""}}
-	}, (error, res, body) => {
-		if(error) {
-			adapter.log.info('error on request: '+ error);
-			return
-		}
-		
-		return body
-	})
-}
+var senecIp;
 
 adapter.on('ready', function () {
-	adapter.subscribeStates('*');
-    main();
-	
-	// read all elements from service
-	const response = readFromServer();
-	adapter.log.info('response on request: '+ response);
-	
-	// subscribe on all variables of this adapter instance with pattern "adapterName.X.*"
-	adapter.setState('STAT_DAY_E_HOUSE', senec.getVarValue('fl_4179F679'), true);
-	adapter.setState('STAT_DAY_E_PV', 2, true);
-	adapter.setState('STAT_DAY_BAT_CHARGE', 3, true);
-	adapter.setState('STAT_DAY_BAT_DISCHARGE', 4, true);
-	adapter.setState('STAT_DAY_E_GRID_IMPORT', 5, true);
-	adapter.setState('STAT_DAY_E_GRID_EXPORT', 6, true);
-	adapter.setState('GUI_BAT_DATA_POWER', 7, true);
-	adapter.setState('GUI_INVERTER_POWER', 8, true);
-	adapter.setState('GUI_HOUSE_POW', 9, true);
-	adapter.setState('STAT_MAINT_REQUIRED', 10, true);
-	adapter.setState('GUI_BAT_DATA_FUEL_CHARGE', 11, true);
-	adapter.setState('GUI_CHARGING_INFO', 12, true);
-	adapter.setState('GUI_BOOSTING_INFO', 13, true);
-	adapter.setState('GUI_GRID_POW', 14, true);
+	adapter.getForeignObject('system.config', function (err, data) {
+        if (data && data.common) {
+            lang  = data.common.language;
+        }
+
+        adapter.log.debug('initializing objects');
+        main();
+
+        setTimeout(function () {
+            adapter.log.info('force terminating adapter after 1 minute');
+            adapter.stop();
+        }, 60000);
+
+    });
 });
 
 adapter.on('objectChange', function (id, obj) {
@@ -113,5 +37,48 @@ adapter.on('stateChange', function (id, state) {
     }
 });
 
-const queueValueParamsets = [];
+function readSettings() {
+    senecIp = adapter.config.senecId;
+    if (senecIp === undefined || senecIp === "") {
+        adapter.log.error('Enter senecIp!'); // Translate!
+        adapter.stop();
+    }  
+    
+    readFromServer();
+}
+
+function readFromServer() {	
+	request.post('http://'+senecIp+'/lala.cgi', {
+		json: {"STATISTIC":{"STAT_DAY_E_HOUSE":"","STAT_DAY_E_PV":"","STAT_DAY_BAT_CHARGE":"","STAT_DAY_BAT_DISCHARGE":"","STAT_DAY_E_GRID_IMPORT":"","STAT_DAY_E_GRID_EXPORT":"","STAT_YEAR_E_PU1_ARR":""},"ENERGY":{"STAT_STATE":"","STAT_STATE_DECODE":"","GUI_BAT_DATA_POWER":"","GUI_INVERTER_POWER":"","GUI_HOUSE_POW":"","GUI_GRID_POW":"","STAT_MAINT_REQUIRED":"","GUI_BAT_DATA_FUEL_CHARGE":"","GUI_CHARGING_INFO":"","GUI_BOOSTING_INFO":""},"WIZARD":{"CONFIG_LOADED":"","SETUP_NUMBER_WALLBOXES":"","SETUP_WALLBOX_SERIAL0":"","SETUP_WALLBOX_SERIAL1":"","SETUP_WALLBOX_SERIAL2":"","SETUP_WALLBOX_SERIAL3":""}}
+	}, (error, res, body) => {
+		if(error) {
+			adapter.log.info('error on request: '+ error);
+			return
+		}
+		
+		// output response to log
+		adapter.log.info('got feedback from senec sevice: ' + body);
+		
+		// subscribe on all variables of this adapter instance with pattern "adapterName.X.*"
+		adapter.setState('STAT_DAY_E_HOUSE', senec.getVarValue('fl_4179F679'), true);
+		adapter.setState('STAT_DAY_E_PV', 2, true);
+		adapter.setState('STAT_DAY_BAT_CHARGE', 3, true);
+		adapter.setState('STAT_DAY_BAT_DISCHARGE', 4, true);
+		adapter.setState('STAT_DAY_E_GRID_IMPORT', 5, true);
+		adapter.setState('STAT_DAY_E_GRID_EXPORT', 6, true);
+		adapter.setState('GUI_BAT_DATA_POWER', 7, true);
+		adapter.setState('GUI_INVERTER_POWER', 8, true);
+		adapter.setState('GUI_HOUSE_POW', 9, true);
+		adapter.setState('STAT_MAINT_REQUIRED', 10, true);
+		adapter.setState('GUI_BAT_DATA_FUEL_CHARGE', 11, true);
+		adapter.setState('GUI_CHARGING_INFO', 12, true);
+		adapter.setState('GUI_BOOSTING_INFO', 13, true);
+		adapter.setState('GUI_GRID_POW', 14, true);
+	})
+}
  
+function main() {
+    readSettings();
+    adapter.log.info('objects written');
+    //adapter.stop();
+}
